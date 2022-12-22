7d553bbd3bf299e441ede6737ed7f1d6bf197031
tenith <hasintha199@gmail.com>
2022-12-22 23:06:36 +0530

    Add initial project

---
+++ nltk_utils.py
@@ -0,0 +1,10 @@
+import nltk
+nltk.download('punkt')
+def tokenize (sentence):
+    return nltk.word_tokenize(sentence)
+
+def Stem (word):
+    pass
+
+def bag_of_words(tokenized_sentence, all_word):
+    pass
