54187349f2fbfd73bd123313e3d82b3f3aea8b54
tenith <hasintha199@gmail.com>
2022-12-22 23:27:13 +0530

    implement nltk

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,10 +1,21 @@
 import nltk
-nltk.download('punkt')
+# nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
 def tokenize (sentence):
     return nltk.word_tokenize(sentence)
 
 def Stem (word):
-    pass
+    return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_word):
     pass
+
+# a = "How long does shipping take ?"
+# print(a)
+# a = tokenize(a)
+# print(a)
+
+words = ["Organize","organizes","organizing"]
+stemmed_word = [Stem(w) for w in words]
+print(stemmed_word)
